4de9bec34123bcc566f570a94ce02638729b80ce
Manuel Díaz <manuelj.diazf@gmail.com>
2023-07-21 14:15:27 -0400

    Variable names changed from spanish to english

--- scripts/clean.py
+++ scripts/clean.py
@@ -4,12 +4,12 @@ import os
 import requests
 
 def main():
-    zona = os.getenv("CERTBOT_ZONE_ID")
+    zone = os.getenv("CERTBOT_ZONE_ID")
     authentication = os.getenv("CERTBOT_BEARER_TOKEN")
     api_key = os.getenv("CERTBOT_API_KEY")
-    correo = os.getenv("CERTBOT_EMAIL")
+    email = os.getenv("CERTBOT_EMAIL")
     headers = {
-        "X-Auth-Email": correo, 
+        "X-Auth-Email": email, 
         "X-Auth-Key": api_key
     }
     if authentication:
@@ -17,10 +17,10 @@ def main():
     if not os.path.isfile("/tmp/ids"):
         print("File not found, TXT records assumed to be cleaned.")
         exit()
-    archivo = open("/tmp/ids", "r")
-    ids = archivo.read().splitlines()
+    file = open("/tmp/ids", "r")
+    ids = file.read().splitlines()
     for id in ids:
-        r = requests.delete("https://api.cloudflare.com/client/v4/zones/" + zona + "/dns_records/" + id, headers=headers)
+        r = requests.delete("https://api.cloudflare.com/client/v4/zones/" + zone + "/dns_records/" + id, headers=headers)
         if (not r.status_code in range(200, 299)):
             print("Error " + str(r.status_code))
             print(r.json())
--- scripts/hook.py
+++ scripts/hook.py
@@ -5,35 +5,35 @@ import requests
 import time
 
 def main():
-    dominio = os.getenv("CERTBOT_DOMAIN")
-    zona = os.getenv("CERTBOT_ZONE_ID")
+    domain = os.getenv("CERTBOT_DOMAIN")
+    zone = os.getenv("CERTBOT_ZONE_ID")
     authentication = os.getenv("CERTBOT_BEARER_TOKEN")
     api_key = os.getenv("CERTBOT_API_KEY")
-    correo = os.getenv("CERTBOT_EMAIL")
-    contenido = os.getenv("CERTBOT_VALIDATION")
-    cuerpo = { 
+    email = os.getenv("CERTBOT_EMAIL")
+    content = os.getenv("CERTBOT_VALIDATION")
+    body = { 
         "type": "TXT", 
-        "name": "_acme-challenge." + dominio, 
-        "content": contenido, 
+        "name": "_acme-challenge." + domain, 
+        "content": content, 
         "ttl": 60 
     }
     headers = {
-        "X-Auth-Email": correo, 
+        "X-Auth-Email": email, 
         "X-Auth-Key": api_key
     }
     if authentication:
         headers["Authentication"] = "Bearer " + authentication
-    r = requests.post("https://api.cloudflare.com/client/v4/zones/" + zona + "/dns_records", json=cuerpo, headers=headers)
+    r = requests.post("https://api.cloudflare.com/client/v4/zones/" + zone + "/dns_records", json=body, headers=headers)
     if (not r.status_code in range(200, 299)):
         print("Error " + str(r.status_code))
         print(r.json())
         exit()
     id = r.json()["result"]["id"]
-    nueva_linea = os.path.isfile("/tmp/ids")
-    archivo = open("/tmp/ids", "a+")
-    if nueva_linea:
-        archivo.write("\n")
-    archivo.write(id)
+    new_line = os.path.isfile("/tmp/ids")
+    file = open("/tmp/ids", "a+")
+    if new_line:
+        file.write("\n")
+    file.write(id)
     time.sleep(60)
 
 
